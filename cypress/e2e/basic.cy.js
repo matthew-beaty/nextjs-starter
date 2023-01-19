b583afc0e62cc11f17c3f995c1af7cdae17ab958
matthewchase <mattchasebeaty+git@gmail.com>
2023-01-19 15:52:46 -0500

    remove default blog tests

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,9 +1,1 @@
-describe('sample render test', () => {
-  beforeEach(() => {
-    cy.visit('/');
-  });
-
-  it('displays the title text', () => {
-    cy.get('h1').contains('Next.js Blog Theme');
-  });
-});
+describe('sample render test', () => {});
